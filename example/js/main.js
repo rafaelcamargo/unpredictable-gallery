6e638e60f76f36d9896b1e766a225750dc348505
Rafael Camargo <contato@rafaelcamargo.com>
2015-01-01 22:40:53 -0200

    imaging_frames Fixed example page and purged its assets

--- example/js/main.js
+++ example/js/main.js
@@ -1,23 +1,8 @@
-function getWindowWidth(){
-	return window.innerWidth;
-}
-
-function getWindowHeight(){
-	return window.innerHeight;
-}
-
-function getUgalContainerHeight(){
-	return getWindowHeight() * 0.7;
-}
-
 function renderUgal(){
 	ugal.init({
 		container: 'ugalContainer',
-		width: getWindowWidth(),
-		height: getUgalContainerHeight(),
-		hFrames: 5,
-		vFrames: 3,
-		fBorder: 1
+		width: window.innerWidth,
+		height: window.innerHeight*0.7
 	});
 }
 
