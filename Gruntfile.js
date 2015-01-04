217ba71f91a4c1fe89887a0c9c1233065a357037
Rafael Camargo <contato@rafaelcamargo.com>
2015-01-04 14:14:00 -0200

    issue#10 Reorganized example files inside welcome page and included stylus and watch tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,23 +7,39 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        banner: '/****************************************************\n' +
-                '<%= pkg.name %> v<%= pkg.version %>\n' +
-                '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-                'Fork me on Github:\n' +
-                'https://github.com/rafaelcamargo/unpredictablegallery\n' +
-                '*****************************************************/\n'
+        banner: '/*\n' +
+                '* <%= pkg.name %> v<%= pkg.version %> ' +
+                '(<%= grunt.template.today("yyyy-mm-dd") %>)\n' +
+                '* License: <%= pkg.license %>\n' +
+                '* Url: <%= pkg.url %>\n' +
+                '* Repository: <%= pkg.repository.url %>\n' +
+                '*/\n'
       },
       my_target: {
         files: {
           'releases/<%= pkg.name %>-<%= pkg.version %>.min.js': ['src/ugal.js']
         }
       }
+    },
+    stylus: {
+      compile: {
+        files: {
+          'welcome/assets/css/main.css': ['welcome/assets/styl/*.styl']
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: 'welcome/assets/styl/*.styl',
+        tasks: ['stylus']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['jshint']);
 };
